68be996a297606ebf1e60d8e820f239e8fc98b8b
ziyi <sheeploveart@gmail.com>
2022-10-27 21:16:44 +0800

    <Dev> Move userData to constants

---
+++ pages/constants.ts
@@ -0,0 +1,48 @@
+export const userData = {
+    bgImg: "./user-portrait.jpeg",
+    name: "Matthew Henry",
+    occupation: "Music Enthusiast",
+    preference: [
+        {id: 1, type: "Looking for competitions"},
+        {id: 2, type: "Discuss in guitar group"},
+        {id: 3, type: "Study finger style online courses"},
+        {id: 4, type: "Contribute music scores"}
+    ],
+    followedMusicians: [
+        {
+            id: 1,
+            name: "Jimmy Hendrix",
+            image: "./jimmy-hendrix.jpeg"
+        },
+        {
+            id: 2,
+            name: "Stevie Ray Vaughan",
+            image: "./jimmy-hendrix.jpeg"
+        },
+        {
+            id: 3,
+            name: "Eddie Van Halen",
+            image: "./jimmy-hendrix.jpeg"
+        },
+        {
+            id: 4,
+            name: "Greg Ginn",
+            image: "./jimmy-hendrix.jpeg"
+        },
+        {
+            id: 5,
+            name: "Kirk Hammett",
+            image: "./jimmy-hendrix.jpeg"
+        },
+        {
+            id: 6,
+            name: "Johnny Marr",
+            image: "./jimmy-hendrix.jpeg"
+        },
+        {
+            id: 7,
+            name: "Neil Young",
+            image: "./jimmy-hendrix.jpeg"
+        }
+    ]
+}
--- pages/user.tsx
+++ pages/user.tsx
@@ -1,54 +1,6 @@
 import type {NextPage} from 'next'
 import Template from "../components/shared/Template"
-
-const userData = {
-    bgImg: "./user-portrait.jpeg",
-    name: "Matthew Henry",
-    occupation: "Music Enthusiast",
-    preference: [
-        {id: 1, type: "Looking for competitions"},
-        {id: 2, type: "Discuss in guitar group"},
-        {id: 3, type: "Study finger style online courses"},
-        {id: 4, type: "Contribute music scores"}
-    ],
-    followedMusicians: [
-        {
-            id: 1,
-            name: "Jimmy Hendrix",
-            image: "./jimmy-hendrix.jpeg"
-        },
-        {
-            id: 2,
-            name: "Stevie Ray Vaughan",
-            image: "./jimmy-hendrix.jpeg"
-        },
-        {
-            id: 3,
-            name: "Eddie Van Halen",
-            image: "./jimmy-hendrix.jpeg"
-        },
-        {
-            id: 4,
-            name: "Greg Ginn",
-            image: "./jimmy-hendrix.jpeg"
-        },
-        {
-            id: 5,
-            name: "Kirk Hammett",
-            image: "./jimmy-hendrix.jpeg"
-        },
-        {
-            id: 6,
-            name: "Johnny Marr",
-            image: "./jimmy-hendrix.jpeg"
-        },
-        {
-            id: 7,
-            name: "Neil Young",
-            image: "./jimmy-hendrix.jpeg"
-        }
-    ]
-}
+import { userData } from './constants'
 
 const User: NextPage = () => {
     return (
